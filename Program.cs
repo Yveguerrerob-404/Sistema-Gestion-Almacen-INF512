18c0e2022e3458081b92cf24ef5ab546ada86c3f
Yveguerrerob-404 <yveguerrerob@gmail.com>
2026-03-05 16:05:04 -0400

    Update 1 Program.cs
    Aquí implementamos la clase Almacen con la lógica de negocio para gestionar la lista de productos.

--- Program.cs
+++ Program.cs
@@ -23,3 +23,35 @@ namespace SistemaGestionComercial
             return $"CÓDIGO: {Codigo.PadRight(10)} | NOMBRE: {Nombre.PadRight(15)} | PRECIO: RD${Precio:N2}";
         }
     }
+     // clase de logica del negocio
+        public class Almacen
+    {
+        // utilizamos List<Producto> 
+        private List<Producto> productos = new List<Producto>();
+
+        public bool AgregarProducto(Producto nuevo)
+        {
+            // Evitar códigos duplicados (Requisito obligatorio)
+            if (productos.Any(p => p.Codigo.Equals(nuevo.Codigo, StringComparison.OrdinalIgnoreCase)))
+                return false;
+
+            productos.Add(nuevo);
+            return true;
+        }
+
+        public Producto? BuscarProducto(string codigo)
+        {
+            return productos.FirstOrDefault(p => p.Codigo.Equals(codigo, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public bool EliminarProducto(string codigo)
+        {
+            var p = BuscarProducto(codigo);
+            if (p != null) return productos.Remove(p);
+            return false;
+        }
+
+        public List<Producto> ObtenerListado() => productos;
+
+        public int ObtenerTotal() => productos.Count;
+    }
